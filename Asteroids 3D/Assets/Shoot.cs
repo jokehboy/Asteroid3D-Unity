7c0885d90f615508ba955f87e77f06d05303473c
jokehboy <jokehboy@gmail.com>
2018-02-02 16:54:13 +0000

    0.0.2
    Added Projectile

---
+++ Asteroids 3D/Assets/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject projectile;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10000);
+
+        }
+    }
+}
