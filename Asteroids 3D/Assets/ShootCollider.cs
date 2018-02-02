4bc9dbeb0a385442359c46d2c51d4f0a8892748e
jokehboy <jokehboy@gmail.com>
2018-02-02 22:22:36 +0000

    Revert "Revert "0.0.2""
    This reverts commit 63268de67c789384f838be27238a84899d7162b2, reversing
    changes made to 97b28923e0ad72dbb0320556a69ec70fc730687a.

---
+++ Asteroids 3D/Assets/ShootCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Asteroid1")
+        {
+            GameObject hit = col.gameObject;
+            AteroidSplit split = hit.GetComponent<AteroidSplit>();
+
+            split.AsteroidDiv();
+            
+
+            Destroy(this.gameObject);
+
+            
+        }
+
+
+    }
+}
