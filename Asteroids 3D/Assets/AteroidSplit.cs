a668aaf833c28ca1c5612f19d9e34dc83262c9e4
jokehboy <jokehboy@gmail.com>
2018-02-06 21:19:50 +0000

    Revert "Revert "Revert "0.0.3"""
    This reverts commit d4baace8b1e8f07535214679e11a8c497ab0350f, reversing
    changes made to e9210b26d61b3f5172d022b800736a4e9faed64e.

---
+++ Asteroids 3D/Assets/AteroidSplit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AteroidSplit : MonoBehaviour {
+
+    public GameObject AsterSplit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+        
+    public void AsteroidDiv()
+        {
+            Instantiate(AsterSplit, transform.position + Random.insideUnitSphere *3f , Quaternion.identity);
+        AsterSplit.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(Random.Range(1, 100), Random.Range(1, 100), Random.Range(1, 100)), new Vector3(Random.value, Random.value, Random.value));
+
+            Instantiate(AsterSplit, transform.position + Random.insideUnitSphere*3f , Quaternion.identity);
+        AsterSplit.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(Random.Range(1,100), Random.Range(1, 100), Random.Range(1, 100)), new Vector3(Random.value, Random.value, Random.value));
+        Destroy(this.gameObject);
+        }
+
+    
+
+}
